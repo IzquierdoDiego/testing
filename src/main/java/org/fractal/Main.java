8344ea2ecb7589b403ffb4aa18103eb1c4c06097
Diego Izquierdo <izquierdodiegof@gmail.com>
2024-07-05 21:24:31 -0500

    TDD

--- src/main/java/org/fractal/Main.java
+++ src/main/java/org/fractal/Main.java
@@ -3,5 +3,9 @@ package org.fractal;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        double salary = 1000;
+        System.out.println(MoneyUtil.format(salary));
+
     }
 }
